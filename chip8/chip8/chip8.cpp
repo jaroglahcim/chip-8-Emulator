32c6494e935482ae33372dbef277fa115d0ffd09
jaroglahcim <49529073+jaroglahcim@users.noreply.github.com>
2019-12-02 00:18:40 +0100

    Add files via upload
    Beta 1.01

--- chip8/chip8/chip8.cpp
+++ chip8/chip8/chip8.cpp
@@ -4,6 +4,9 @@
 #include <time.h>
 #include <windows.h>
 
+#define VX V[(opcode & 0x0F00) >> 8]
+#define VY V[(opcode & 0x00F0) >> 4]
+
 void gotoxy(int x, int y)
 {
 	COORD c;
@@ -102,7 +105,7 @@ void Chip8::emulateCycle()
 
 		case 0x3000:														// 3XNN: Skips the next instruction if VX equals NN.
 																			// (Usually the next instruction is a jump to skip a code block)
-			if (V[(opcode & 0x0F00) >> 8] == (opcode & 0x00FF))				// if (V[X] == NN)
+			if (VX == (opcode & 0x00FF))									// if (V[X] == NN)
 				pc += 4;
 			else
 				pc += 2;
@@ -110,7 +113,7 @@ void Chip8::emulateCycle()
 
 		case 0x4000:														// 4XNN: Skips the next instruction if VX doesn't equal NN.
 																			// (Usually the next instruction is a jump to skip a code block)
-			if (V[(opcode & 0x0F00) >> 8] != (opcode & 0x00FF))				// if (V[X] != NN)
+			if (VX != (opcode & 0x00FF))									// if (V[X] != NN)
 				pc += 4;
 			else
 				pc += 2;
@@ -124,19 +127,19 @@ void Chip8::emulateCycle()
 				pc += 2;
 				break;
 			}
-			if (V[(opcode & 0x0F00) >> 8] == V[(opcode & 0x00F0) >> 4])		// if (V[X] == V[Y])
+			if (VX == VY)		// if (V[X] == V[Y])
 				pc += 4;
 			else
 				pc += 2;
 		break;
 
 		case 0x6000:														// 6XNN: Sets VX to NN.
-			V[(opcode & 0x0F00) >> 8] = opcode & 0x00FF;
+			VX = opcode & 0x00FF;
 			pc += 2;
 		break;
 
 		case 0x7000:														// 7XNN: Adds NN to VX. (Carry flag is not changed) 
-			V[(opcode & 0x0F00) >> 8] += opcode & 0x00FF;
+			VX += opcode & 0x00FF;
 			pc += 2;
 		break;
 
@@ -144,82 +147,78 @@ void Chip8::emulateCycle()
 			switch (opcode & 0x000F)
 			{
 				case 0x0000:												// 8XY0: Sets VX to the value of VY. 
-					V[(opcode & 0x0F00) >> 8] = V[(opcode & 0x00F0) >> 4];	// V[X] = V[Y]
+					VX = VY;	
 					pc += 2;
 				break;
 
 				case 0x0001:												// 8XY1 : Sets VX to VX or VY. (Bitwise OR operation)
-					V[(opcode & 0x0F00) >> 8] |= V[(opcode & 0x00F0) >> 4];	// V[X] = V[X] | V[Y]
+					VX |= VY;	
 					pc += 2;
 				break;
 
 				case 0x0002:												// 8XY2 : Sets VX to VX and VY. (Bitwise AND operation)
-					V[(opcode & 0x0F00) >> 8] &= V[(opcode & 0x00F0) >> 4];	// V[X] = V[X] & V[Y]
+					VX &= VY;	
 					pc += 2;
 				break;
 
 				case 0x0003:												// 8XY3 : Sets VX to VX xor VY. (Bitwise XOR operation)
-					V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];	// V[X] = V[X] ^ V[Y]
+					VX ^= VY;	
 					pc += 2;
 				break;
 
 				case 0x0004:												// 8XY4 : Adds VY to VX. VF is set to 1 when there's a carry, 
 																			// and to 0 when there isn't.
-					if (V[(opcode & 0x00F0) >> 4] > 						// V[Y] > 0xFF - V[X] <=>  V[Y] + V[X] > 0xFF
-					   (0xFF - V[(opcode & 0x0F00) >> 8]))					// (but there won't be an overflow on 1-byte long variables)
-					{
+					if (VY > (0xFF - VX))									// V[Y] > 0xFF - V[X] <=>  V[Y] + V[X] > 0xFF
+					{  														// (but there won't be an overflow on 1-byte long variables)
 						V[0xF] = 1;											// Carry Flag = 1, there's a carry
 					}
 					else
 					{
 						V[0xF] = 0;											// Carry Flag = 0
 					}
-					V[(opcode & 0x0F00) >> 8] += V[(opcode & 0x00F0) >> 4]; // V[X] = V[X] + V[Y]
+					VX += VY; 
 					pc += 2;
 				break;
 
-				case 0x0005:												// VY is subtracted from VX. VF is set to 0 when there's a borrow, 
+				case 0x0005:												// 8XY5: VY is subtracted from VX. VF is set to 0 when there's a borrow, 
 																			// and 1 when there isn't.
-					if ((V[(opcode & 0x0F00) >> 8]) < 						// V[X] < V[Y] <=>  V[X] - V[Y] < 0
-						(V[(opcode & 0x00F0) >> 4]))						// (but there won't be an underflow on 1-byte long variables)
-					{
+					if ((VX) < (VY))										// V[X] < V[Y] <=>  V[X] - V[Y] < 0
+					{														// (but there won't be an underflow on 1-byte long variables)
 						V[0xF] = 0;											// Borrow Flag = 0, there's a borrow
 					}
 					else
 					{
 						V[0xF] = 1;											// Borrow Flag = 1, there's no borrow
 					}
-					V[(opcode & 0x0F00) >> 8] -= V[(opcode & 0x00F0) >> 4]; // V[X] = V[X] - V[Y]
+					VX -= VY; 
 					pc += 2;
 				break;
 
-				case 0x0006:												// 8XY3 : Stores the least significant bit of VX in VF
+				case 0x0006:												// 8XY6 : Stores the least significant bit of VX in VF
 																			// and then shifts VX to the right by 1
-					V[0xF] = V[(opcode & 0x0F00) >> 8] & 0x0001;			// V[F] = V[X] & 0x0001 (least significant bit of VX)
-					V[(opcode & 0x0F00) >> 8] >>= 1;						// Shift VX to the right by 1
+					V[0xF] = VX & 0x0001;									// V[F] = V[X] & 0x0001 (least significant bit of VX)
+					VX >>= 1;												// Shift VX to the right by 1
 					pc += 2;
 				break;
 
-				case 0x0007:												// Sets VX to VY minus VX. VF is set to 0 when there's a borrow,  
+				case 0x0007:												// 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow,  
 																			// and 1 when there isn't.
-					if ((V[(opcode & 0x0F00) >> 8]) < 						// V[Y] < V[X] <=>  V[Y] - V[X] < 0
-						(V[(opcode & 0x00F0) >> 4]))						// (but there won't be an underflow on 1-byte long variables)
-					{
+					if (VY < VX)											// V[Y] < V[X] <=>  V[Y] - V[X] < 0
+					{														// (but there won't be an underflow on 1-byte long variables)
 						V[0xF] = 0;											// Borrow Flag = 0, there's a borrow
 					}
 					else
 					{
 						V[0xF] = 1;											// Borrow Flag = 1, there's no borrow
 					}
-					V[(opcode & 0x0F00) >> 8] =	V[(opcode & 0x00F0) >> 4]	// V[X] = V[Y] - V[X]
-						- V[(opcode & 0x0F00) >> 8]; 
+					VX = VY - VX; 
 					pc += 2;
 				break;
 
 				case 0x000E:												// 8XYE : Stores the most significant bit of VX in VF
 																			// and then shifts VX to the left by 1
-					V[0xF] = (V[(opcode & 0x0F00) >> 8]) >> 7;				// V[F] = most significant bit of V[X]
-					V[(opcode & 0x0F00) >> 8] <<= 1;						// Shift VX to the left by 1
+					V[0xF] = (VX) >> 7;										// V[F] = most significant bit of V[X]
+					VX <<= 1;												// Shift VX to the left by 1
 					pc += 2;
 				break;
 
@@ -239,7 +238,7 @@ void Chip8::emulateCycle()
 				pc += 2;
 				break;
 			}
-			if (V[(opcode & 0x0F00) >> 8] != V[(opcode & 0x00F0) >> 4])		// if (V[X] != V[Y])
+			if (VX != VY)		// if (V[X] != V[Y])
 				pc += 4;
 			else
 				pc += 2;
@@ -256,7 +255,7 @@ void Chip8::emulateCycle()
 
 		case 0xC000:														// CXNN: Sets VX to the result of a bitwise and operation
 																			// on a random number (Typically: 0 to 255) and NN.
-			V[(opcode & 0x0F00) >> 8] = (rand() % 0xFF) & (opcode & 0x00FF);
+			VX = (rand() % 0xFF) & (opcode & 0x00FF);
 			pc += 2;
 		break;
 
@@ -266,8 +265,8 @@ void Chip8::emulateCycle()
 			as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. VF is set to 1
 			if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen */
 
-			unsigned char xStart = V[(opcode & 0x0F00) >> 8];
-			unsigned char yStart = V[(opcode & 0x00F0) >> 4];
+			unsigned char xStart = VX;
+			unsigned char yStart = VY;
 			unsigned char height = opcode & 0x000F;
 			unsigned char pixels;
 
@@ -300,7 +299,7 @@ void Chip8::emulateCycle()
 			{
 				case 0x009E:												// EX9E: Skips the next instruction	if the key
 																			// stored in VX is pressed
-					if (key[V[(opcode & 0x0F00) >> 8]] != 0)				// if key[V[X]] != 0
+					if (key[VX] != 0)										// if key[V[X]] != 0
 						pc += 4;
 					else
 						pc += 2;
@@ -308,7 +307,7 @@ void Chip8::emulateCycle()
 
 				case 0xA1:													// EXA1: Skips the next instruction if the key
 																			// stored in VX isn't pressed.
-					if (key[V[(opcode & 0x0F00) >> 8]] == 0)				// if key[V[X]] == 0
+					if (key[VX] == 0)										// if key[V[X]] == 0
 						pc += 4;
 					else
 						pc += 2;
@@ -320,7 +319,7 @@ void Chip8::emulateCycle()
 			switch (opcode & 0x00FF)
 			{
 				case 0x0007:												// FX07: Sets VX to the value of the delay timer.
-					V[(opcode & 0x0F00) >> 8] = delay_timer;
+					VX = delay_timer;
 					pc += 2;
 				break;
 
@@ -330,7 +329,7 @@ void Chip8::emulateCycle()
 					{
 						if (key[i] != 0)									 
 						{
-							V[(opcode & 0x0F00) >> 8] = i;					// If a key is pressed, send its number to VX and read next opcode
+							VX = i;											// If a key is pressed, send its number to VX and read next opcode
 							pc += 2;										// on next cycle; else the same opcode will be read on next cycle
 							break;											// until a key is pressed
 						}
@@ -338,47 +337,46 @@ void Chip8::emulateCycle()
 				break;
 
 				case 0x0015:												// FX15: Sets the delay timer to VX. 
-					delay_timer = V[(opcode & 0x0F00) >> 8];
+					delay_timer = VX;
 					pc += 2;
 				break;
 
 				case 0x0018:												// FX18: Sets the sound timer to VX. 
-					sound_timer = V[(opcode & 0x0F00) >> 8];
+					sound_timer = VX;
 					pc += 2;
 				break;
 
 				case 0x001E:												// FX18: Adds VX to I.
-					if ((I + V[(opcode & 0x0F00) >> 8]) > 0xFFF)			// VF is set to 1 when there is a range overflow (I+VX>0xFFF),
+					if ((I + VX) > 0xFFF)									// VF is set to 1 when there is a range overflow (I+VX>0xFFF),
 						V[0xF] = 1;											// and to 0 when there isn't. This is an undocumented feature of
 					else													// the CHIP-8 and used by the Spacefight 2091! game.
 						V[0xF] = 0;
-					I += V[(opcode & 0x0F00) >> 8];
+					I += VX;
 					pc += 2;
 				break;
 
-				case 0x0029:												// Sets I to the location of the sprite for the character in VX.
+				case 0x0029:												// FX29: Sets I to the location of the sprite for the character in VX.
 																			// Characters 0 - F(in hexadecimal) are represented by a 4x5 font.
-					I = memory[FONTSET_START +
-						5 * V[(opcode & 0x0F00) >> 8]];
+					I = memory[FONTSET_START + 5 * VX];
 					pc += 2;
 				break;
 
-				case 0x0033:												// Takes the decimal representation of VX, places the hundreds digit
+				case 0x0033:												// FX33: Takes the decimal representation of VX, places the hundreds digit
 																			// in memory at location in I, the tens digit at location I + 1,
 																			// and the ones digit at location I + 2
-					memory[I] = (V[(opcode & 0x0F00) >> 8]) / 100;
-					memory[I+1] = (V[(opcode & 0x0F00) >> 8] % 100) / 10;
-					memory[I+2] = (V[(opcode & 0x0F00) >> 8] % 10);
+					memory[I] = (VX) / 100;
+					memory[I+1] = (VX % 100) / 10;
+					memory[I+2] = (VX % 10);
 					pc += 2;
 				break;
 
-				case 0x0055:												// Stores V0 to VX (including VX) in memory starting at address I
+				case 0x0055:												// FX55: Stores V0 to VX (including VX) in memory starting at address I
 					for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)     
 						memory[I + i] = V[i];
 					pc += 2;
 				break;
 
-				case 0x0065:												// Fills V0 to VX (including VX) from memory starting at address I								
+				case 0x0065:												// FX65: Fills V0 to VX (including VX) from memory starting at address I								
 					for (int i = 0; i <= ((opcode & 0x0F00) >> 8); ++i)
 						V[i] = memory[I + i];
 					pc += 2;
